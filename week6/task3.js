1f74115d1fc3179cf87232d7fe08656c3f08fb33
gaurav3444 <3444gauravsharma@gmail.com>
2022-08-28 12:17:07 +1000

    week 6

---
+++ week6/task3.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            selected: "",
+            dynamicValues: [1, 2, 3, 4, 5, 6, 7, 8],
+            dynamicSelected: "",
+
+        }
+    },
+})
